b62336328645025668169cb1e45a034ef88e5eaa
melike2d <gino@femboy.email>
2022-01-24 14:22:45 -0800

    feat: remove dynamic content-type, add support for files.

--- encoding.go
+++ encoding.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/ugorji/go/codec"
 )
 
 var codecHandle = codec.MsgpackHandle{}
 
-func Encode(contentType string, data interface{}) ([]byte, error) {
-	if contentType == "application/msgpack" {
-		bytes := new([]byte)
-		return *bytes, codec.NewEncoderBytes(bytes, &codecHandle).Encode(data)
-	} else if contentType == "application/json" {
-		return json.Marshal(data)
-	}
-
-	return nil, errors.New("Unsupported Content-Type")
+func Encode(data interface{}) ([]byte, error) {
+	bytes := new([]byte)
+	return *bytes, codec.NewEncoderBytes(bytes, &codecHandle).Encode(data)
 }
 
-func Decode(contentType string, data []byte, result interface{}) error {
-	switch contentType {
-	case "application/msgpack":
-		return codec.NewDecoderBytes(data, &codecHandle).Decode(result)
-	case "application/json":
-		return json.Unmarshal(data, result)
-	default:
-		panic(fmt.Sprintf("Unknown content-type: %s", contentType))
-	}
+func Decode(data []byte, result interface{}) error {
+	return codec.NewDecoderBytes(data, &codecHandle).Decode(result)
 }
--- routes.go
+++ routes.go
@@ -8,6 +8,11 @@ import (
 	"mixtape.gg/betsu/kantoku/discord"
 )
 
+type KantokuReply struct {
+	Headers *map[string]string `json:"headers"`
+	Body    []byte             `json:"body"`
+}
+
 func GetIndex(c *fiber.Ctx) error {
 	return c.JSON(createJson("Hello, World!", true))
 }
@@ -59,7 +64,13 @@ func handleInteraction(c *fiber.Ctx) error {
 		}
 
 		if resp != nil {
-			return c.Status(fiber.StatusOK).JSON(resp)
+			if resp.Headers != nil {
+				for key, value := range *resp.Headers {
+					c.Set(key, value)
+				}
+			}
+
+			return c.Status(fiber.StatusOK).Send(resp.Body)
 		}
 	} else {
 		log.Debugln("Received Ping")
@@ -70,19 +81,17 @@ func handleInteraction(c *fiber.Ctx) error {
 	})
 }
 
-func publishInteraction(i *discord.Interaction) (*discord.InteractionResponse, error) {
-	contentType := config.GetDefault("kantoku.content-type", "application/msgpack").(string)
-
+func publishInteraction(i *discord.Interaction) (*KantokuReply, error) {
 	/* encode the interaction so that it can be sent to the message queue */
-	body, err := Encode(contentType, i)
+	body, err := Encode(i)
 	if err != nil {
 		return nil, err
 	}
 
 	/* publish the interaction and wait for a reply */
-	resp, err := Amqp.Call("INTERACTION_CREATE", amqp091.Publishing{
+	resp, err := Amqp.Call(InteractionsEvent, amqp091.Publishing{
 		Body:        body,
-		ContentType: contentType,
+		ContentType: "application/msgpack",
 	})
 
 	if err != nil {
@@ -101,12 +110,8 @@ func publishInteraction(i *discord.Interaction) (*discord.InteractionResponse, e
 		return nil, err
 	}
 
-	if resp.ContentType != "" {
-		contentType = resp.ContentType
-	}
-
-	response := new(discord.InteractionResponse)
-	if err := Decode(contentType, resp.Body, &response); err != nil {
+	response := new(KantokuReply)
+	if err := Decode(resp.Body, &response); err != nil {
 		return nil, err
 	}
 
